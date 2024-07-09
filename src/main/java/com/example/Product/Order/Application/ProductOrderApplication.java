06b97f1e7824f7004ae65bb35d30cb8a7a48887d
Affan-Ahad <affanahad870@gmail.com>
2024-07-08 20:51:31 -0700

    Initial code commit

---
+++ src/main/java/com/example/Product/Order/Application/ProductOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.Product.Order.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderApplication.class, args);
+	}
+
+}
